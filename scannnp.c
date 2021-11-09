4380756d38f7c5caaf1528f3312a54db5ffcafde
20A95A3503 <93142101+20A95A3503@users.noreply.github.com>
2021-11-09 12:20:55 +0530

    Add files via upload

--- scannnp.c
+++ scannnp.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 void main()
 {
-	int a,b,c,d,p;
-	printf("enter four numbers");
-	scanf("%d%d%d%d",&a,&b,&c,&b);
-	p=a*b*c*d;
-	printf("product of %d ,%d,%d,%d",p,a,b,c,d);
+	int A;
+	printf("enter a number",A);
+	scanf("%d",&A);
+	printf("corresponding ASCII character,A");
 }
